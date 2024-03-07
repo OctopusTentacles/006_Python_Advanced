bac96e961ddeab606701d58df68797c26949de27
Octopus <klinov.artem@gmail.com>
2024-03-07 12:54:34 +0600

    M_02 HW1 com3 BASE_DIR

--- Module_02/homework/hw1/get_summary_rss.py
+++ Module_02/homework/hw1/get_summary_rss.py
@@ -13,12 +13,27 @@ $ ps aux > output_file.txt
 Это означает, что ответ надо перевести в байты, килобайты, мегабайты и так далее.
 """
 
+import os
+
 
 def get_summary_rss(ps_output_file_path: str) -> str:
-    ...
+    with open(ps_output_file_path, 'r') as output_file:
+        lines = output_file.readlines()[1:]
+
+        total_memory = 0
+
+        for line in lines:
+            columns = line.split()
+            total_memory += int(columns[5])
+
+    units = ['Б', 'Кб', 'Мб', 'Гб', 'Тб']
+    cur_unit_index = 0
+        
+    return total_memory
 
 
 if __name__ == '__main__':
-    path: str = 'PATH_TO_OUTPUT_FILE'
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    path: str = os.path.join(BASE_DIR, 'output_file.txt')
     summary_rss: str = get_summary_rss(path)
     print(summary_rss)
