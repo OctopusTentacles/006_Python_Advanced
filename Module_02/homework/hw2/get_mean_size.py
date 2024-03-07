62cf2966f084b2594e533e502df44a103508b2b1
Octopus <klinov.artem@gmail.com>
2024-03-07 16:02:39 +0600

    M_02 HW2 com4

--- Module_02/homework/hw2/get_mean_size.py
+++ Module_02/homework/hw2/get_mean_size.py
@@ -1,11 +1,14 @@
 """
-Удобно направлять результат выполнения команды напрямую в программу с помощью конвейера (pipe):
+Удобно направлять результат выполнения команды напрямую в программу 
+с помощью конвейера (pipe):
 
 $ ls -l | python3 get_mean_size.py
 
-Напишите функцию get_mean_size, которая на вход принимает результат выполнения команды ls -l,
+Напишите функцию get_mean_size, которая на вход принимает 
+результат выполнения команды ls -l,
 а возвращает средний размер файла в каталоге.
 """
+
 
 import sys
 
@@ -13,14 +16,18 @@ import sys
 def get_mean_size(ls_output: str) -> float:
 
     total_memory = 0
-    count = 0
+    count_files = 0
 
     for line in ls_output:
-        columns = line.split()
-        total_memory += int(columns[4])
-        count += 1
+        # учитываем только файлы:
+        if line.startswith('-'):
+            columns = line.split()
+            total_memory += int(columns[4])
+            count_files += 1
 
-    return total_memory / count if count > 0 else 0
+    # на пустой директории - ZeroDivisionError;
+    # поэтому добавим условие:
+    return total_memory / count_files if count_files > 0 else 0
 
 if __name__ == '__main__':
     data: str = sys.stdin.readlines()[1:]
