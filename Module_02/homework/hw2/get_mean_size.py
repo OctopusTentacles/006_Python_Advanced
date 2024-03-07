9e6e00bfc7820b00e8a4f2a107814a209315880d
Octopus <klinov.artem@gmail.com>
2024-03-07 15:32:10 +0600

    M_02 HW2 com2 get_mean_size / ls -l

--- Module_02/homework/hw2/get_mean_size.py
+++ Module_02/homework/hw2/get_mean_size.py
@@ -11,13 +11,19 @@ import sys
 
 
 def get_mean_size(ls_output: str) -> float:
+    print(ls_output)
 
     total_memory = 0
     count = 0
 
-    return ls_output
+    for line in ls_output:
+        columns = line.split()
+        total_memory += int(columns[4])
+        count += 1
+
+    return total_memory / count
 
 if __name__ == '__main__':
-    data: str = sys.stdin.read()
+    data: str = sys.stdin.readlines()[1:]
     mean_size: float = get_mean_size(data)
     print(mean_size)
