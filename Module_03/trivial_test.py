2424c8e761e37e2fee62100c8219dc27f0ca2acf
Octopus <klinov.artem@gmail.com>
2024-03-24 20:19:46 +0500

    3_3_1 trivial_test

--- Module_03/trivial_test.py
+++ Module_03/trivial_test.py
@@ -5,3 +5,11 @@ def check_if_can_get_child_status():
     age = 8
     expected_result = 'child'
     function_result = get_social_status(age)
+    if function_result == expected_result:
+        print('it WORKS')
+    else:
+        print('it DOES NOT works')
+
+
+if __name__ == '__main__':
+    check_if_can_get_child_status()
