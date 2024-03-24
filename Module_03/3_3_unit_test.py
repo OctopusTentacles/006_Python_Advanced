26b5f9ca7d762a379ee08213e70c714444d34dfc
Octopus <klinov.artem@gmail.com>
2024-03-24 18:45:20 +0500

    3_3 social_age

---
+++ Module_03/3_3_unit_test.py
@@ -0,0 +1,16 @@
+def get_social_status(age):
+    if not isinstance(age, (int, float)):
+        raise ValueError('Please, provide a number')
+    
+    if age < 0:
+        raise ValueError('Check age!')
+    elif 0 < age < 13:
+        return 'child'
+    elif 13 < age < 18:
+        return 'teenager'
+    elif 18 < age < 50:
+        return 'adult'
+    elif 50 < age < 65:
+        return 'elderly'
+    else:
+        return 'pensioner'
