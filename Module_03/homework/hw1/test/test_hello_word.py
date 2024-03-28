79e6fe1e9451907729fe7846f6b5e14c040afe8a
Octopus <klinov.artem@gmail.com>
2024-03-28 16:04:57 +0500

    M03_hw1_com6 freeze_time

--- Module_03/homework/hw1/test/test_hello_word.py
+++ Module_03/homework/hw1/test/test_hello_word.py
@@ -13,7 +13,22 @@ class TestHelloWordWithDay(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['DEBUG'] = False
         self.app = app.test_client()
+        self.base_url = '/'
+        self.base_url = '/hello-world/'
+
+    def test_has_username(self):
+        username = 'username'
+        response = self.app.get(self.base_url + username)
+        response_text = response.data.decode()
+        self.assertTrue(username in response_text)
         
+    # заморозим день 2024-04-01 - понедельник:
+    @freeze_time('2024-04-01')
+    def test_weekday(self):
+        response = self.app.get(self.base_url + 'username')
+        response_text = response.data.decode()
+        expected_greeting = 'Хорошего понедельника'
+        self.assertIn(expected_greeting, response_text)
 
 
 
