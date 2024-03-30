cb51d7f85dfc8f21a0061cffd3085a74af063d20
Octopus <klinov.artem@gmail.com>
2024-03-30 14:51:41 +0500

    M03_hw1_com14 custom_greeting

--- Module_03/homework/hw1/test/test_hello_word_with_day.py
+++ Module_03/homework/hw1/test/test_hello_word_with_day.py
@@ -1,4 +1,20 @@
-""" Тест для Задачи 1 - Хорошего дня! """
+""" Тест для Задачи 1 - Хорошего дня! 
+
+TestCase - для проверки ожидаемых результатов.
+
+setUp - для настройки тестового окружения перед выполнением каждого теста. 
+        Этот метод будет вызван перед каждым тестом, и вы можете использовать
+        его для инициализации состояния или ресурсов, 
+        необходимых для выполнения тестов.
+
+Каждый метод начинается с префикса test_, 
+что позволяет unittest обнаруживать эти методы как тесты автоматически. 
+Внутри каждого метода выполняются проверки 
+с использованием методов assert* из unittest.
+
+..F. -  каждая точка представляет успешно пройденный тест, 
+        а F представляет неудачный (тест с ошибкой).
+"""
 
 
 import unittest
@@ -9,6 +25,11 @@ from freezegun import freeze_time
 
 
 class TestHelloWordWithDay(unittest.TestCase):
+    """
+    TestHelloWordWithDay является подклассом unittest.TestCase, 
+    что обеспечивает его совместимость с фреймворком для автоматизированного 
+    тестирования unittest. 
+    """
     def setUp(self) -> None:
         app.config['TESTING'] = True
         app.config['DEBUG'] = False
@@ -20,26 +41,56 @@ class TestHelloWordWithDay(unittest.TestCase):
         response = self.app.get(self.base_url + username)
         response_text = response.data.decode()
         self.assertTrue(username in response_text)
+
+    def test_
         
     # заморозим день 2024-04-01 - понедельник:
     @freeze_time('2024-04-01')
-    def test_weekday(self):
+    def test_monday(self):
         response = self.app.get(self.base_url + 'username')
         response_text = response.data.decode()
         expected_greeting = 'Хорошего понедельника'
         self.assertIn(expected_greeting, response_text)
 
-    @freeze_time('2024-04-07')
-    def test_weekday(self):
+    @freeze_time('2024-04-02')
+    def test_tuesday(self):
         response = self.app.get(self.base_url + 'username')
         response_text = response.data.decode()
         expected_greeting = 'Хорошего вторника'
         self.assertIn(expected_greeting, response_text)
 
     @freeze_time('2024-04-03')
-    def test_weekday(self):
+    def test_wednesday(self):
         response = self.app.get(self.base_url + 'username')
         response_text = response.data.decode()
-        expected_greeting = 'Хорошей среды'
+        expected_greeting = 'Хорошего воскресенья'
+        self.assertIn(expected_greeting, response_text)
+
+    @freeze_time('2024-04-04')
+    def test_thursday(self):
+        response = self.app.get(self.base_url + 'username')
+        response_text = response.data.decode()
+        expected_greeting = 'Хорошего четверга'
+        self.assertIn(expected_greeting, response_text)
+
+    @freeze_time('2024-04-05')
+    def test_fryday(self):
+        response = self.app.get(self.base_url + 'username')
+        response_text = response.data.decode()
+        expected_greeting = 'Хорошей пятницы'
+        self.assertIn(expected_greeting, response_text)
+
+    @freeze_time('2024-04-06')
+    def test_saturday(self):
+        response = self.app.get(self.base_url + 'username')
+        response_text = response.data.decode()
+        expected_greeting = 'Хорошей субботы'
+        self.assertIn(expected_greeting, response_text)
+
+    @freeze_time('2024-04-03')
+    def test_sunday(self):
+        response = self.app.get(self.base_url + 'username')
+        response_text = response.data.decode()
+        expected_greeting = 'Хорошего воскресенья'
         self.assertIn(expected_greeting, response_text)
 
