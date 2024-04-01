23af59ed731ea1184b2af91f86c7d8008c99bfda
Octopus <klinov.artem@gmail.com>
2024-04-01 10:06:01 +0500

    M03_hw1_com20

--- Module_03/hw1/tests/test_hello_word_with_day.py
+++ Module_03/hw1/tests/test_hello_word_with_day.py
@@ -20,7 +20,7 @@ setUp - для настройки тестового окружения пере
 import unittest
 
 
-from hello_word_with_day import app
+from Module_03.hw1.hello_word_with_day import app
 from freezegun import freeze_time
 
 
