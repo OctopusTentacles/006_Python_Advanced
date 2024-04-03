73e554b35e78186928eeb999879fc25913337ffd
Octopus <klinov.artem@gmail.com>
2024-04-03 15:47:28 +0500

    M03_hw2_com3 test_decrypt

--- Module_03/homework/hw2/tests/test_decrypt.py
+++ Module_03/homework/hw2/tests/test_decrypt.py
@@ -8,6 +8,7 @@ from Module_03.homework.hw2.decrypt import decrypt
 
 class TestDecrypt(unittest.TestCase):
 
+    # отдельные функции____________________________________________________:
     def test_one_dot(self):
         encrypted_text = 'абра-кадабра.'
         expected_decrypted_text = 'абра-кадабра'
@@ -19,5 +20,23 @@ class TestDecrypt(unittest.TestCase):
         self.assertEqual(decrypt(encrypted_text), expected_decrypted_text)
 
 
+    # контекст-менеджер subTest___________________________________________:
+    def test_decrypt(self):
+        encrypted_texts = [
+            ('абра-кадабра.', 'абра-кадабра'),
+            ('абраа..-кадабра', 'абра-кадабра'),
+            (' абраа..-.кадабра', ' абра-кадабра'),
+            ('абра--..кадабра', 'абра-кадабра'),
+            ('абрау...-кадабра', 'абра-кадабра'),
+            ('абра........', ''),
+            ('абр......a.', 'a'),
+            ('1..2.3', '23'), 
+            ('.', ''),
+            ('1.......................', '')
+        ]
+        for encrypted_text, expected_text in encrypted_texts:
+            with self.subTest(encrypted_text=encrypted_text):
+                decrypted_text = decrypt(encrypted_text)
+                self.assertEqual(decrypted_text, expected_text)
 
 
