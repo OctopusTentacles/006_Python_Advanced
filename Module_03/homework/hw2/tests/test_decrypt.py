dd76a7bb04f02dba9850052be441eaaad7a1d26b
Octopus <klinov.artem@gmail.com>
2024-04-03 15:11:19 +0500

    M03_hw2_com2 test_two_dots

--- Module_03/homework/hw2/tests/test_decrypt.py
+++ Module_03/homework/hw2/tests/test_decrypt.py
@@ -9,12 +9,15 @@ from Module_03.homework.hw2.decrypt import decrypt
 class TestDecrypt(unittest.TestCase):
 
     def test_one_dot(self):
-        encrypted_text = "абра-кадабра."
-        expected_decrypted_text = "абра-кадабра"
+        encrypted_text = 'абра-кадабра.'
+        expected_decrypted_text = 'абра-кадабра'
+        self.assertEqual(decrypt(encrypted_text), expected_decrypted_text)
+
+    def test_two_dots(self):
+        encrypted_text = 'абраа..-кадабра'
+        expected_decrypted_text = 'абра-кадабраы'
         self.assertEqual(decrypt(encrypted_text), expected_decrypted_text)
 
 
 
 
-if __name__ == '__main__':
-    unittest.main()
