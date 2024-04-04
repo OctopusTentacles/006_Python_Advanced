07970cdbbb9e76af874d42ba02378984fdf65b9c
Octopus <klinov.artem@gmail.com>
2024-04-04 15:07:26 +0500

    M03_hw3_com4 test_add working

--- Module_03/homework/hw3/tests/test_accounting.py
+++ Module_03/homework/hw3/tests/test_accounting.py
@@ -4,19 +4,23 @@
 import unittest
 
 from accounting import app
+from accounting import storage
+# from Module_03.homework.hw3.accounting import app
+# from Module_03.homework.hw3.accounting import storage
 
 
 class TestAccounting(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        # Инициализация данных
         cls.app = app.test_client()
-        cls.storage = {
+
+        # Инициализация данных
+        storage.update({
             2023: {1: {1: 100, 'month_total': 100}, 'year_total': 100},
             2024: {4: {4: 200, 5: 300, 'month_total': 500}, 'year_total': 500}
-        }
+        })
 
     def test_add(self):
-        responce = self.app.get('/add/20240406/200')
+        responce = self.app.get('/add/20240506/200')
         self.assertEqual(responce.status_code, 200)
