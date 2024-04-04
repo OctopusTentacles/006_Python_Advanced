4872b6992abac0bec290b8f5a91ddb581c123f4a
Octopus <klinov.artem@gmail.com>
2024-04-04 14:39:22 +0500

    M03_hw3_com3 test_add

--- Module_03/homework/hw3/tests/test_accounting.py
+++ Module_03/homework/hw3/tests/test_accounting.py
@@ -1,1 +1,22 @@
 """ Тест для Учета финансов. """
+
+
+import unittest
+
+from accounting import app
+
+
+class TestAccounting(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        # Инициализация данных
+        cls.app = app.test_client()
+        cls.storage = {
+            2023: {1: {1: 100, 'month_total': 100}, 'year_total': 100},
+            2024: {4: {4: 200, 5: 300, 'month_total': 500}, 'year_total': 500}
+        }
+
+    def test_add(self):
+        responce = self.app.get('/add/20240406/200')
+        self.assertEqual(responce.status_code, 200)
