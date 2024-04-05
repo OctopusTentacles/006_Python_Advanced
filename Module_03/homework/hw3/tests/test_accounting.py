1c1f4e6cfc4159dca4c3b803c9ac2de5181b9b87
Octopus <klinov.artem@gmail.com>
2024-04-05 21:42:58 +0500

    M03_hw3_com12

--- Module_03/homework/hw3/tests/test_accounting.py
+++ Module_03/homework/hw3/tests/test_accounting.py
@@ -18,8 +18,8 @@ class TestAccounting(unittest.TestCase):
 
         # Инициализация данных
         storage.update({
-            # 2023: {1: {1: 100, 'month_total': 100}, 'year_total': 100},
-            # 2024: {4: {4: 200, 5: 300, 'month_total': 500}, 'year_total': 500}
+            2023: {1: {1: 100, 'month_total': 100}, 'year_total': 100},
+            2024: {4: {4: 200, 5: 300, 'month_total': 500}, 'year_total': 500}
         })
 
     def test_add_correct(self):
@@ -37,14 +37,23 @@ class TestAccounting(unittest.TestCase):
             add('20240232', 200)
 
 
-    # если в `storage` ничего нет и блок try-ecxept закомментирован - ERROR.
+# ===========================================================================
     def test_calculate_year(self):
-        responce = self.app.get('/calculate/2023')  #100
+        responce = self.app.get('/calculate/2023')  # 100
         self.assertEqual(responce.status_code, 200)
+
+    # если в `storage` ничего нет и блок try-ecxept закомментирован - ERROR.
+    def test_calculate_year_empty_storage(self):
+        storage.clear()
+        response = self.app.get('/calculate/2023')  
+        self.assertEqual(response.status_code, 500)
     # если в `storage` ничего нет и блок try-ecxept в работе - 
-
-
-
+    def test_calculate_year_empty_storage_with_try(self):
+        storage.clear()
+        response = self.app.get('/calculate/2023')
+        response_text = response.data.decode()
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('В эту дату нет трат!', response_text)
 
 
 
