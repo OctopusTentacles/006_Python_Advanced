04809bf54dd8aa0a200ee3f65814e40aa1747d3e
Octopus <klinov.artem@gmail.com>
2024-04-07 20:57:20 +0500

    M03_hw4_com6 test_get_age

--- Module_03/homework/hw4/tests/test_person.py
+++ Module_03/homework/hw4/tests/test_person.py
@@ -9,3 +9,14 @@ from person import Person
 
 
 class TestPerson(unittest.TestCase):
+    name = 'Boby'
+    birth_year = 2000
+    address = '17 Baker str.'
+    person = Person(name, birth_year, address)
+
+    def test_get_age(self):
+        current_year = datetime.datetime.now().year
+        expected_age = current_year - self.birth_year
+        self.assertEqual(self.person.get_age(), expected_age)
+
+
