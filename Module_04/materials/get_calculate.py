0ed796eabfee81863081df80e0de3b17874591b2
Octopus <klinov.artem@gmail.com>
2024-04-19 20:29:46 +0500

    M04_com7 get_calculate

--- Module_04/materials/get_calculate.py
+++ Module_04/materials/get_calculate.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from typing import List, Optional
 
 
 app = Flask(__name__)
@@ -6,6 +7,16 @@ app = Flask(__name__)
 
 @app.route('/calculate/', methods=['GET'])
 def calculate():
+    # Получение чисел из параметра запроса:
+    numbers_str:  List[str] = request.args.get('numbers')
+    if not numbers_str:
+        return jsonify({'error': 'Please provide a list of numbers.'}), 400
+    
+    # преобразуем строку в список чисел:
+    try:
+        numbers = [int(num) for num in numbers_str.split(',')]
+    except ValueError:
+        return jsonify({'error': 'Invalid input. Please provide a list of integers.'}), 400
     ...
 
 
