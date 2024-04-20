406e5682f0107396ec0d782934bfc4acdedd0e11
Octopus <klinov.artem@gmail.com>
2024-04-20 20:18:34 +0500

    M04_com14 get_number

--- Module_04/materials/get_number_X.py
+++ Module_04/materials/get_number_X.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 
 @app.route('/number', methods=['GET'])
 def number():
+    # получаем отсортированный массив из чисел и число:
+    A: List[int] = sorted(request.args.get('A'))
+    X: int = int(request.args.get('X'))
+
+    # Проверяем, что массив A и число X были предоставлены:
+    if not A or X is None:
+        return jsonify({'error': 'Please provide array A and a number X'}), 400
     ...
 
 
