846d6cc4f30e7b943c4026066dde0622b0efb3dd
Octopus <klinov.artem@gmail.com>
2024-04-22 15:00:27 +0500

    M04_com19 flask_post

--- Module_04/materials/flask_post.py
+++ Module_04/materials/flask_post.py
@@ -4,10 +4,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/number', methods=['POST'])
+@app.route('/sum', methods=['POST'])
 def _sum():
-    ...
+    array1 = request.form.getlist('array1', type=int)
+    array2 = request.form.getlist('array2', type=int)
 
+    result = ','.join(str(a1 + a2) for (a1, a2) in zip(array1, array2))
+
+    return f'array of sums is: {result}'
 
 
 if __name__ == '__main__':
