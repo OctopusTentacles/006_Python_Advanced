63e5b00d97a3ec39702ba598803e7726ef797049
Octopus <klinov.artem@gmail.com>
2024-04-22 17:36:31 +0500

    M04_com20 flask_post

--- Module_04/materials/flask_post.py
+++ Module_04/materials/flask_post.py
@@ -1,3 +1,6 @@
+import json
+from urllib.parse import unquote_plus
+
 from flask import Flask, request
 
 
@@ -11,7 +14,15 @@ def _sum():
 
     result = ','.join(str(a1 + a2) for (a1, a2) in zip(array1, array2))
 
-    return f'array of sums is: {result}'
+    return f'Array of sums is: [{result}]'
+
+
+@app.route('/sum2', methods=['POST'])
+def _sum2():
+    form_data = request.get.data(as_text=True)
+    print(f'form_data = {form_data}')
+
+    return 'OK'
 
 
 if __name__ == '__main__':
