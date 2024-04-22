3a8ba0e78c0cd1612512c10dc164941f526c8042
Octopus <klinov.artem@gmail.com>
2024-04-22 14:53:02 +0500

    M04_com18 flask_post

---
+++ Module_04/materials/flask_post.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/number', methods=['POST'])
+def _sum():
+    ...
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
