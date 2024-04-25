300ef4eee1a54722afb3f92a35ec693aaf7cb6c9
Octopus <klinov.artem@gmail.com>
2024-04-25 15:48:46 +0500

    M04_com34 flask_wtf install flask-wtf

--- Module_04/materials/flask_wtform.py
+++ Module_04/materials/flask_wtform.py
@@ -1,9 +1,20 @@
 from flask import Flask, request
+from flask_wtform import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, Email, NumberRange
 
 
 
 app = Flask(__name__)
 
+
+class RegistrationForm(FlaskForm):
+    email = StringField()
+    phone = IntegerField()
+    name = StringField()
+    address = StringField()
+    index = IntegerField()
+    comment = StringField()
 
 
 @app.route('/registration', methods=['POST'])
@@ -17,4 +28,5 @@ def registration():
 
 
 if __name__ == '__main__':
+    app.config['WTF_CSRF_ENABLED'] = False
     app.run(debug=True)
