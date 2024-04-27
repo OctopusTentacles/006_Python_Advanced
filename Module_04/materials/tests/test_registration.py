d7953adbfb48d851c0ac52dda99af391784d5ea5
Octopus <klinov.artem@gmail.com>
2024-04-27 21:46:45 +0500

    M04_com42 test_registration

--- Module_04/materials/tests/test_registration.py
+++ Module_04/materials/tests/test_registration.py
@@ -22,3 +22,15 @@ class BaseRegistrationTest(TestCase):
         return app
     
     def test_email_required(self):
+        response = self.client.post('/registration', data = dict(
+            phone='1234567890',
+            name='Ivanov Ivan',
+            address='Village Ave. My Home',
+            index=12345,
+            comment='Test comment'
+        ))
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('email', response.data.decode())
+
+    def test_phone_required(self):
+        ...
