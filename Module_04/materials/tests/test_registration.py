dcf9d63a4b153c5bade06f359705c95be53b7e4c
Octopus <klinov.artem@gmail.com>
2024-04-29 22:33:20 +0500

    M04_com47 test_registration test_phone_required

--- Module_04/materials/tests/test_registration.py
+++ Module_04/materials/tests/test_registration.py
@@ -6,11 +6,11 @@ from Module_04.materials.flask_wt_form import RegistrationForm
 
 
 class BaseRegistrationTest(unittest.TestCase):
-    email = 'test@example.com',
-    phone = '1234567890',
-    name = 'Ivanov Ivan',
-    address = 'Village Ave. My Home',
-    index = 222111,
+    email = 'test@example.com'
+    phone = 1234567890
+    name = 'Ivanov Ivan'
+    address = 'Village Ave. My Home'
+    index = 222111
     comment = 'Test comment'
 
     def create_app(self):
@@ -20,21 +20,30 @@ class BaseRegistrationTest(unittest.TestCase):
     
     def setUp(self):
         self.app = self.create_app()    
-        
+
     def test_email_required(self):
         client = self.app.test_client()
         response = client.post('/registration', data = dict(
-            phone='1234567890',
-            name='Ivanov Ivan',
-            address='Village Ave. My Home',
-            index=12345,
-            comment='Test comment'
+            phone=self.phone,
+            name=self.name,
+            address=self.address,
+            index=self.index,
+            comment=self.comment
         ))
         self.assertEqual(response.status_code, 400)
         self.assertIn('email', response.data.decode())
 
     def test_phone_required(self):
-        ...
+        client = self.app.test_client()
+        response = client.post('/registration', data = dict(
+            email = self.email,
+            name = self.name,
+            address = self.address,
+            index = self.index,
+            comment = self.comment
+        ))
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('phone', response.data.decode())
 
 
 if __name__ == '__main__':
