1668412c7989a6aab1785ef94cec9c0b59333e68
Octopus <klinov.artem@gmail.com>
2024-04-30 17:13:01 +0500

    M04_com52 test_registration test_index_required

--- Module_04/materials/tests/test_registration.py
+++ Module_04/materials/tests/test_registration.py
@@ -15,7 +15,7 @@ class BaseRegistrationTest(unittest.TestCase):
     phone = 1234567890
     name = 'Ivanov Ivan'
     address = 'Village Ave. My Home'
-    index = 222111
+    index = 'A'
     comment = 'Test comment'
 
     def create_app(self):
@@ -88,6 +88,39 @@ class BaseRegistrationTest(unittest.TestCase):
 
         self.assertIn('address', response.data.decode())
         logging.debug('Response data: %s', response.data.decode())
+
+
+    def test_index_required(self):
+        client = self.app.test_client()
+        response = client.post('/registration', data = dict(
+            email = self.email,
+            phone = self.phone,
+            name = self.name,
+            index = self.index,
+            address = self.address,
+            comment = self.comment
+        ))
+        self.assertEqual(response.status_code, 400)
+        logging.debug('Response status code: %s', response.status_code)
+
+        self.assertIn('index', response.data.decode())
+        logging.debug('Response data: %s', response.data.decode())
+
+    
+    def test_valid_index(self):
+        client = self.app.test_client()
+        response = client.post('/registration', data = dict(
+        email=self.email,
+        phone=self.phone,
+        name=self.name,
+        index=123456,  # Указываем валидный индекс
+        address=self.address,
+        comment=self.comment
+    ))
+        # Ожидаем успешный статус код
+        self.assertEqual(response.status_code, 200)
+        logging.debug('Response status code: %s', response.status_code)
+
 
 
 
