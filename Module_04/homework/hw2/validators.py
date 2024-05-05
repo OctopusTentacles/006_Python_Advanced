d708e9a0c39358c79381a6c289f58352299db704
Octopus <klinov.artem@gmail.com>
2024-05-05 21:20:19 +0500

    M04_hw2_com3 def number_length

--- Module_04/homework/hw2/validators.py
+++ Module_04/homework/hw2/validators.py
@@ -2,10 +2,18 @@ from typing import Optional
 
 from flask_wtf import FlaskForm
 from wtforms import Field
+from wtforms.validators import ValidationError
 
 
 def number_length(min: int, max: int, message: Optional[str] = None):
-    ...
+    def _number_length(form: FlaskForm, field: Field):
+        if field.data is None:
+            return
+        if not (min <= len(str(field.data)) <= max):
+            if message is None:
+                message = f'Number must be between {min} and {max} digits long.'
+            raise ValidationError(message)
+    return _number_length
 
 
 class NumberLength:
