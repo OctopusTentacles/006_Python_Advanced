fe7b12c2bc48415832f235eb9affae6e140028eb
Octopus <klinov.artem@gmail.com>
2024-05-08 16:53:17 +0500

    M04_hw3_com4 test_valid_email

--- Module_04/homework/hw3/test_registration.py
+++ Module_04/homework/hw3/test_registration.py
@@ -6,7 +6,7 @@
 """
 
 import unittest
-from hw1.registration import app
+from Module_04.homework.hw1.registration import app
 
 
 
@@ -15,18 +15,28 @@ class TestRegistration(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
     
-    def test_valid_registration(self):
-        # Создаем корректные данные для регистрации
-        data = {
-            'email': 'test@example.com',
-            'phone': 9876543210,
-            'name': 'Test User',
-            'address': 'Test address',
-            'index': 111999,
-            'comment': ''
-        }
+    def test_valid_email(self):
+        email = 'test@example.com'
+        # Отправляем POST-запрос с корректными данными
+        response = self.app.post('/registration_hw1', email)
+        # Проверяем, что статус код равен 200
+        self.assertEqual(response.status_code, 200)
+        # Проверяем, что ответ содержит сообщение об успешной регистрации
+        self.assertIn('Successfully registered user', response.data.decode())
 
-        
+
+
+
+        # data = {
+        #     'email': 'test@example.com',
+        #     'phone': 9876543210,
+        #     'name': 'Test User',
+        #     'address': 'Test address',
+        #     'index': 111999,
+        #     'comment': ''
+        # }
+
+
 
 
 
