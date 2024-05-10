9320b625975c39f3cbf6f01847b80ff31a6ca690
Octopus <klinov.artem@gmail.com>
2024-05-10 16:50:29 +0500

    M04_hw3_com17

--- Module_04/homework/hw3/tests/test_registration.py
+++ Module_04/homework/hw3/tests/test_registration.py
@@ -132,6 +132,21 @@ class TestRegistration(unittest.TestCase):
         logging.debug("Response data: %s", response.data.decode())
 
 # ===================================================================
+    def test_valid_name(self):
+        client = self.app.test_client()
+        response = client.post('/registration_hw1', data = dict(
+            email=self.email,
+            phone=self.phone,
+            name=self.name,
+            address=self.address,
+            index=self.index,
+            comment=self.comment
+        ))
+        self.assertEqual(response.status_code, 200)
+        logging.debug("Response status code: %s", response.status_code)
+
+        self.assertIn('Successfully registered user', response.data.decode())
+        logging.debug("Response data: %s", response.data.decode())
 
 
 
