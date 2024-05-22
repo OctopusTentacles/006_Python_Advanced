f2973a3d67528d4f5f6b581e6a14f691a479af57
Octopus <klinov.artem@gmail.com>
2024-05-22 21:24:00 +0500

    M05_linux_process_com16 sys.stderr

--- Module_05/materials/processes_from_py/subprocess_run.py
+++ Module_05/materials/processes_from_py/subprocess_run.py
@@ -1,13 +1,21 @@
-import subprocess
+import subprocess, sys
 
 
 def run_program():
     res = subprocess.run(['python', 'test_program.py'], 
                          input=b'some input\notherinput',
-                         stderr=subprocess.PIPE
+                         # захват потоков:
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         text=True
                         )
-    print(res)
+    # Объединяем stdout и stderr и выводим в stderr:
+    output = res.stdout + res.stderr
+
+    # перенаправить весь объединенный вывод в stderr:
+    print(output, file=sys.stderr)
 
 
 if __name__ == '__main__':
     run_program()
+
