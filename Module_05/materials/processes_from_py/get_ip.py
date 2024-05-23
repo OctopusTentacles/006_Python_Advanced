2e3c0769ccb1b738bc44e227f8cf381c9420275d
Octopus <klinov.artem@gmail.com>
2024-05-23 21:40:30 +0500

    M05_linux_process_com18

---
+++ Module_05/materials/processes_from_py/get_ip.py
@@ -0,0 +1,20 @@
+"""
+Напишите функцию, которая с помощью subprocess будет запускать команду:
+$ curl -i -H "Accept: application/json" -X GET https://api.ipify.org?format=json
+- Токенизируйте через shlex, 
+- распарсите вывод и 
+- верните строку — IP-адрес.
+
+"""
+
+
+import shlex
+import subprocess
+import sys
+
+
+def get_ip_address():
+    command = 'curl -i -H "Accept: application/json" -X GET https://api.ipify.org?format=json'
+
+    # токенизация команды:
+    args = shlex.split(command)
