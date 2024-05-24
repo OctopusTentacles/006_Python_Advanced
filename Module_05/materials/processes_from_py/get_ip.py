8fbf74f33ae542da3dec36348c7af89d36e6b1e4
Octopus <klinov.artem@gmail.com>
2024-05-24 17:20:44 +0500

    M05_linux_process_com21

--- Module_05/materials/processes_from_py/get_ip.py
+++ Module_05/materials/processes_from_py/get_ip.py
@@ -20,14 +20,51 @@ def get_ip_address():
     args = shlex.split(command)
 
     # выполнение команды:
-    result = subprocess.run(args)
+    result = subprocess.run(
+        args, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, 
+        text=True
+    )
     
     # Вывод команды
     output = result.stdout
-
-
-
     print(output)
+
+
+    # заголовки :
+    # HTTP/2 200 
+    # date: Thu, 23 May 2024 17:02:30 GMT
+    # content-type: application/json
+    # content-length: 20
+    # vary: Origin
+    # cf-cache-status: DYNAMIC
+    # server: cloudflare
+    # cf-ray: 88869d2659962bcf-FRA
+
+    # тело:
+    # {"ip":"146.23.46.5"}
+
+    # HTTP-ответ состоит из двух частей:
+    # Заголовков (Headers): Каждый заголовок заканчивается \r\n.
+    # Тела (Body): Тело ответа следует после заголовков.
+    # Для того чтобы отделить заголовки от тела, 
+    # используется последовательность \r\n\r\n:
+
+    # Один \r\n завершает последний заголовок.
+    # Второй \r\n указывает, что заголовки закончились и начинается тело.
+
+    # Когда вы используете split('\r\n\r\n', 1), 
+    # вы говорите Python, что хотите разделить строку на две части:
+    # Первая часть будет содержать все заголовки.
+    # Вторая часть будет содержать тело ответа.
+    headers, body = output.split('\r\n\r\n', 1)
+    print(headers)
+    print(body)
+
+
 
 if __name__ == '__main__':
     get_ip_address()
+
+
