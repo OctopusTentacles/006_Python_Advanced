3cd4bfb8f849d4612a0f2d7f49f72b23b64e53d0
Octopus <klinov.artem@gmail.com>
2024-05-26 19:48:22 +0500

    M05_popen_com6 all_sleep

---
+++ Module_05/materials/popen/sleep_15.py
@@ -0,0 +1,20 @@
+"""
+Запустите с помощью объекта Popen команду:
+$ sleep 15 && echo "My mission is done here!"
+Запустите сразу десять таких объектов так, 
+чтобы всё вместе это заняло примерно 15 секунд, но не более 20.
+"""
+
+
+import subprocess
+
+
+def all_sleep():
+
+
+    return
+
+
+if __name__ == '__main__':
+    res = all_sleep()
+    
