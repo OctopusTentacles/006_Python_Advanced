81a57c907239598e53fa178ec8ed21d319c5b2b4
Octopus <klinov.artem@gmail.com>
2024-05-26 20:19:46 +0500

    M05_popen_com7 all_sleep

--- Module_05/materials/popen/sleep_15.py
+++ Module_05/materials/popen/sleep_15.py
@@ -6,15 +6,27 @@ $ sleep 15 && echo "My mission is done here!"
 """
 
 
+import shlex
 import subprocess
 
 
 def all_sleep():
+    command = 'sleep 15 && echo "My mission is done here!"'
 
+    # токенизация команды:
+    token_command = shlex.split(command)
 
-    return
+    # выполнение команды:
+    result = subprocess.Popen(
+        token_command,
+        stdout=subprocess.PIPE,
+        text=True
+    )
+    stdout, _ = result.communicate()
+
+    return stdout
 
 
 if __name__ == '__main__':
+    print('Start!')
     res = all_sleep()
-    
