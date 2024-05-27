4e49c9c2240075a1ac63f7795c19e569c4689a0d
Octopus <klinov.artem@gmail.com>
2024-05-27 17:30:54 +0500

    M05_popen_com3 sleep_exit

--- Module_05/materials/popen/exit_popen.py
+++ Module_05/materials/popen/exit_popen.py
@@ -2,6 +2,13 @@
 Запустите такую команду, используя объект Popen: 
 $ sleep 10 && exit 1
 
+
+Попробуйте:
+Подождать объект девять секунд и проверить, что он не был завершён. 
+Чтобы это сделать, вам нужно будет передать в метод wait тайм-аут
+https://docs.python.org/3/library/subprocess.html#subprocess.Popen.wait.
+Подождать без тайм-аута и проверить, что программа вернула код 1.
+
 """
 
 
@@ -17,20 +24,23 @@ def sleep_exit():
     # токенизация команды:
     token_command = shlex.split(command) #['sleep', '10', '&&', 'exit', '1']
 
-    sleep_command = token_command[:2]
-    exit_command = token_command[3:]
+    # sleep_command = token_command[:2]
+    # exit_command = token_command[3:]
 
     # запуск sleep:
-    sleep_processes = subprocess.Popen(sleep_command)
+    process = subprocess.Popen(token_command, shell=True)
     # ожидание завершения sleep:
-    sleep_processes.wait()
+    process.wait()
 
     exit_process = 1
 
-    
-    return exit_process
+    return_code = process.returncode
+
+    print(f'Process finished with return code: {return_code}')
 
 
 if __name__ == '__main__':
-    exit_code = sleep_exit()
-    print(f'Process exited with code {exit_code}')
+    start_time = time.time()
+    sleep_exit()
+    end_time = time.time()
+    print(f'Total time taken: {end_time - start_time} sec.')
