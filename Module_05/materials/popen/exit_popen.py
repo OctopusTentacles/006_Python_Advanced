613e406b82870cea108f5164517f37666b31197b
Octopus <klinov.artem@gmail.com>
2024-05-27 17:18:11 +0500

    M05_popen_com2 sleep_exit

--- Module_05/materials/popen/exit_popen.py
+++ Module_05/materials/popen/exit_popen.py
@@ -15,10 +15,22 @@ def sleep_exit():
     command = 'sleep 10 && exit 1'
 
     # токенизация команды:
-    token_command = shlex.split(command)
-    print(token_command)
+    token_command = shlex.split(command) #['sleep', '10', '&&', 'exit', '1']
 
+    sleep_command = token_command[:2]
+    exit_command = token_command[3:]
+
+    # запуск sleep:
+    sleep_processes = subprocess.Popen(sleep_command)
+    # ожидание завершения sleep:
+    sleep_processes.wait()
+
+    exit_process = 1
+
+    
+    return exit_process
 
 
 if __name__ == '__main__':
-    sleep_exit()
+    exit_code = sleep_exit()
+    print(f'Process exited with code {exit_code}')
