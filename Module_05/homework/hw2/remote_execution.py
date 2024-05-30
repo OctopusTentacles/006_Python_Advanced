f7d19b469a9738030b9d84471e3f0f5ca352ac6a
Octopus <klinov.artem@gmail.com>
2024-05-30 17:13:43 +0500

    M05_hw2_com4

--- Module_05/homework/hw2/remote_execution.py
+++ Module_05/homework/hw2/remote_execution.py
@@ -9,27 +9,30 @@
 """
 
 
+import subprocess
+
+
 from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, NumberRange
 
 
 app = Flask(__name__)
 
 
 class CodeForm(FlaskForm):
-    code = StringField()
-    timeout = IntegerField()
+    code = StringField(validators=[InputRequired()])
+    timeout = IntegerField(validators=[NumberRange(min=1, max=30)])
 
 
 def run_python_code_in_subproccess(code: str, timeout: int):
-    ...
-
+    try:
+        process = subprocess.Popen()
 
 @app.route('/run_code', methods=['POST'])
 def run_code():
-    ...
-
+    form = CodeForm()
 
 if __name__ == '__main__':
     app.run(debug=True)
