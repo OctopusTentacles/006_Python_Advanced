e7555e127b576ffa89112a1220158534a83d4d33
Octopus <klinov.artem@gmail.com>
2024-06-02 09:31:14 +0500

    M05_hw2_com13 test create_app

--- Module_05/homework/hw2/test_remote_execution.py
+++ Module_05/homework/hw2/test_remote_execution.py
@@ -1,5 +1,23 @@
 import unittest
+import logging
 from remote_execution import app
+
+
+# Настройка конфигурации логгирования
+logging.basicConfig(level=logging.DEBUG)
+
+
+class RemoteExecution(unittest.TestCase):
+    code = print('Hello, World!')
+    timeout = 3
+
+    def create_app(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        return app
+
+
+    def test_timeout(self):
 
 
 if __name__ == '__main__':
