304a7ebd456a9a84a61f08e83f2a0051c7f0c122
Octopus <klinov.artem@gmail.com>
2024-06-07 20:49:16 +0500

    M05_hw3_com3 test prepare

---
+++ Module_05/homework/hw3/test_block_errors.py
@@ -0,0 +1,13 @@
+import logging, unittest
+
+from block_errors import BlockErrors
+
+
+# Настройка конфигурации логгирования
+logging.basicConfig(level=logging.DEBUG)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
