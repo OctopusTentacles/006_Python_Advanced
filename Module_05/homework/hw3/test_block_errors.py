efebd78f3cb8f9a9b038240c829a6f7a2ce02373
Octopus <klinov.artem@gmail.com>
2024-06-08 11:19:04 +0500

    M05_hw3_com7 test block errors

--- Module_05/homework/hw3/test_block_errors.py
+++ Module_05/homework/hw3/test_block_errors.py
@@ -7,6 +7,16 @@ from block_errors import BlockErrors
 logging.basicConfig(level=logging.DEBUG)
 
 
+class TestBlockErrors(unittest.TestCase):
+    
+    def test_ignor_error(self):
+        """Ошибка игнорируется."""
+        err_types = {ZeroDivisionError, TypeError}
+        with BlockErrors(err_types):
+            a = 1 / 0
+        print('Выполнено без ошибок')
+
+
 
 
 if __name__ == '__main__':
