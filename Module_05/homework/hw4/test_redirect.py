55aaec31263d2e8f9c9f67da1396ba4875fae708
Octopus <klinov.artem@gmail.com>
2024-06-11 20:41:09 +0500

    M05_hw4_com7

--- Module_05/homework/hw4/test_redirect.py
+++ Module_05/homework/hw4/test_redirect.py
@@ -1,11 +1,18 @@
+import os
 import unittest
 from redirect import Redirect
 
 
+cur_dir = os.path.dirname(__file__)
+
 class TestRedirect(unittest.TestCase):
 
     def test_redirect_stdout(self):
-        
+        with open(os.path.join(cur_dir, 'stdout.txt'), 'w') as f_out,\
+            open(os.path.join(cur_dir, 'stderr.txt'), 'w') as f_err:
+            with Redirect(stdout=f_out, stderr=f_err):
+                
+
 
 
 if __name__ == '__main__':
