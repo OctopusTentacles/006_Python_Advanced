420ed4861d0b4497956488de1a26253726fd91c0
Octopus <klinov.artem@gmail.com>
2024-06-12 20:50:59 +0500

    M05_hw4_com9 setUp

--- Module_05/homework/hw4/test_redirect.py
+++ Module_05/homework/hw4/test_redirect.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import unittest
+
 from redirect import Redirect
 
 
@@ -7,12 +9,23 @@ cur_dir = os.path.dirname(__file__)
 
 class TestRedirect(unittest.TestCase):
 
+    def setUp(self):
+        """Сохраняем оригинальные значения потоков."""
+        self.original_stdout = sys.stdout
+        self.original_stderr = sys.stderr
+
     def test_redirect_stdout(self):
         with open(os.path.join(cur_dir, 'stdout.txt'), 'w') as f_out,\
             open(os.path.join(cur_dir, 'stderr.txt'), 'w') as f_err:
             with Redirect(stdout=f_out, stderr=f_err):
                 print('Это стандартный поток вывода')
+                # проверяем, что sys.stdout и sys.stderr были перенаправлены:
+                self.assertNotEqual(sys.stdout, self.original_stdout)
+                self.assertNotEqual(sys.stderr, self.original_stderr)
 
+    
+    def test_redirect_stderr(self):
+        ...
 
 
 
