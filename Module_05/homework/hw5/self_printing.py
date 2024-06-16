8025fc1b37b5ede4bb1e7464a9de7540ebfb664d
Octopus <klinov.artem@gmail.com>
2024-06-16 19:28:25 +0500

    M05_hw5_com3 self print

--- Module_05/homework/hw5/self_printing.py
+++ Module_05/homework/hw5/self_printing.py
@@ -7,4 +7,12 @@ result = 0
 for n in range(1, 11):
     result += n ** 2
 
+
 # Secret magic code
+import sys
+
+
+with open(sys.argv[0], 'r') as py_file:
+    py_file_content = py_file.read()
+
+print(py_file_content)
