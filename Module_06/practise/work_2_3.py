bddb5d54c4f592304c037373b936206363f1f4fe
Octopus <klinov.artem@gmail.com>
2024-07-09 16:22:09 +0500

    M06_6.2_com18

--- Module_06/practise/work_2_3.py
+++ Module_06/practise/work_2_3.py
@@ -28,11 +28,35 @@ from werkzeug.exceptions import InternalServerError
 app = Flask(__name__)
 @app.route('/caiculate/', methods=['POST'])
 def calculate():
+    expression = request.json.get('expression')
+    try:
+        # eval принимает строку, интерпретирует её как 
+        # арифметическое выражение и возвращает результат этого выражения:
+        # # Вычисляем результат выражения:
+        result = eval(expression)
+        # Возвращаем результат в формате JSON:
+        return jsonify(result=result)
+    except ZeroDivisionError as exc:
+        raise ZeroDivisionError(str(exc))
+    except FloatingPointError as exc:
+        raise FloatingPointError(str(exc))
+    except OverflowError as exc:
+        raise OverflowError(str(exc))
+    except ArithmeticError as exc:
+        raise ArithmeticError(str(exc))
+    
+# Обработчик ошибок для ArithmeticError:
 
 
+# Обработчик ошибок для ZeroDivisionError:
+
+
+# Обработчик ошибок для FloatingPointError:
+
+
+# Обработчик ошибок для OverflowError:
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    expression = request.json.get('expression')
