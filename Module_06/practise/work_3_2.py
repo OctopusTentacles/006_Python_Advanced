f1590e37d23f15c93fcbbbe1cc27a43bb36f01fd
Octopus <klinov.artem@gmail.com>
2024-07-13 22:02:22 +0500

    M06_6.3_com5

---
+++ Module_06/practise/work_3_2.py
@@ -0,0 +1,43 @@
+"""
+Количество попыток ввода неправильного пароля у нас 
+строго зашито в коде программы, это плохо.
+Пусть наша программа будет чуть более вежливой и спросит, 
+сколько раз пользователь хочет ввести пароль.
+
+Минимальное количество раз — два, максимальное — десять.
+
+В случае возникновения ошибок нужно, конечно, 
+правильным образом их залогировать.
+
+"""
+
+
+import getpass
+import hashlib
+import logging
+
+
+logger = logging.getLogger('password_checker')
+
+def input_and_check_password():
+    logger.debug('Нчало функции ввода пароля\n')
+    password: str = getpass.getpass()
+
+
+
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    logger.debug('Вы пытаетесь авторизоваться\n')
+    
+    count_number: int = 3
+    logger.info(f'У вас {count_number} попыток\n')
+
+    while count_number > 0:
+        if input_and_check_password():
+            exit(0)
+        count_number -= 1
+
+    logger.error('Пользователь использовал все попытки!\n')
+    exit(1)
