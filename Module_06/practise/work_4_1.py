77cc42573e607025ec20efae631d452121588c6c
Octopus <klinov.artem@gmail.com>
2024-07-19 16:52:08 +0500

    M06_6.4_com2

--- Module_06/practise/work_4_1.py
+++ Module_06/practise/work_4_1.py
@@ -9,6 +9,7 @@
 
 
 import csv
+import logging
 import os
 
 from flask import Flask
@@ -17,11 +18,11 @@ from werkzeug.exceptions import InternalServerError
 
 
 cur_dir = os.path.dirname(__file__)
-
+logger = logging.getLogger('bank_api')
 
 app = Flask(__name__)
 
-@app.rout('/bank_api/<branch>/<int:person_id>')
+@app.route('/bank_api/<branch>/<int:person_id>')
 def bank_api(branch: str, person_id: int):
 
     branch_card_file_name = os.path.join(cur_dir, f'bank_data/{branch}.csv')
@@ -31,8 +32,10 @@ def bank_api(branch: str, person_id: int):
 
         for record in csv_reader:
             if int(record['id']) == person_id:
+                logger.info(f'Найден пользователь с ID {person_id}')
                 return record['name']
             else:
+                logger.info(f'Пользователь с ID {person_id} не найден')
                 return 'Person not found', 404
 
 @app.errorhandler(InternalServerError)
@@ -40,18 +43,16 @@ def handle_ecxeption(e: InternalServerError):
     original: Optional[Exception] = getattr(e, 'original_exception', None)
 
     if isinstance(original, FileNotFoundError):
-        with open(os.path.join(cur_dir, 'invalid_error.log'), 'a') as fo:
-            fo.write(
-                f'Tried to access {original.filename}. Exception info: {original.strerror}\n'
-            )
+        logger.debug(f'Tried to access {original.filename}. Exception info: {original.strerror}\n')
+
     elif isinstance(original, OSError):
-        with open(os.path.join(cur_dir, 'invalid_error.log'), 'a') as fo:
-            fo.write(
-                f'Unable to access a card. Exception info: {original.strerror}\n'
-            )
+        logger.debug(f'Unable to access a card. Exception info: {original.strerror}\n')
+
+    else:
+        logger.error(f'An unexpected internal server error occurred: {e}')
     return 'Internal server error', 500
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app.run()
-    
