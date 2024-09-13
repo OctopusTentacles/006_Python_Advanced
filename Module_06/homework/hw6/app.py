5f92a9594d7c8029372efce89a9485e21bfc988e
Octopus <klinov.artem@gmail.com>
2024-09-13 19:23:46 +0500

    M06_hw6_com3 has_no_empty_params

--- Module_06/homework/hw6/app.py
+++ Module_06/homework/hw6/app.py
@@ -36,7 +36,9 @@ def index():
 
 # ===================================================================
 def has_no_empty_params(rule):
-
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 
 # обработчик ошибки 404:
 @app.errorhandler(404)
