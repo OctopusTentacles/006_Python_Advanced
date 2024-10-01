4c4a0a2a7cba0c8a95bdd57d7dabe01386effcf6
Octopus <klinov.artem@gmail.com>
2024-10-01 09:59:02 +0500

    M07_logger_object_com9

--- Module_07/materials/logger_object/http_utils.py
+++ Module_07/materials/logger_object/http_utils.py
@@ -3,19 +3,21 @@ import time
 
 import requests
 
-logger = logging.getLogger('http_utils')
+
+logger_utils = logging.getLogger('logger_utils')
+logger_http_utils = logging.getLogger('logger_utils.http_utils')
 
 GET_IP_URL = 'https://api.ipify.org?format=json'
 
 
 def get_ip_address() -> str:
-    logger.debug('Start getting IP address')
+    logger_http_utils.debug('Start getting IP address')
     start = time.time()
     try:
         ip = requests.get(GET_IP_URL).json()['ip']
     except Exception as e:
-        logger.exception(e)
+        logger_http_utils.exception(e)
         raise e
-    logger.debug('Done requesting ip in {:.4f} seconds'.format(time.time() - start))
-    logger.info('Ip address: {}'.format(ip))
+    logger_http_utils.debug('Done requesting ip in {:.4f} seconds'.format(time.time() - start))
+    logger_http_utils.info('Ip address: {}'.format(ip))
     return ip
