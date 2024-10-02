21acf6fbbe4c7d3591bfe77ea820144cd90bd9f8
Octopus <klinov.artem@gmail.com>
2024-10-02 13:03:44 +0500

    M07_basic_config_com6

--- Module_07/materials/basic_configuration/02_tasks.py
+++ Module_07/materials/basic_configuration/02_tasks.py
@@ -18,7 +18,19 @@ sub_sub_1.setLevel(logging.DEBUG)
 
 
 def main():
-    ...
+    print(root.handlers)
+    root.debug('This is a root debug message\n')
+
+    print(sub_1.handlers)
+    sub_1.debug('This debug message from sub_1 should not appear')
+    sub_1.info('This is an info message from sub_1')
+
+    print(sub_2.handlers)
+    sub_2.info('This is an info message from sub_2')
+
+    print(sub_sub_1.handlers)
+    sub_sub_1.debug('This is a debug message from sub_sub_1')
+    sub_sub_1.info('This is an info message from sub_sub_1')
 
 
 if __name__ == '__main__':
