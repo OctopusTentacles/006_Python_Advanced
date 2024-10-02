27e34714d0f0594fb6602a80822d2484502ececb
Octopus <klinov.artem@gmail.com>
2024-10-02 14:01:13 +0500

    M07_basic_config_com7

--- Module_07/materials/basic_configuration/02_tasks.py
+++ Module_07/materials/basic_configuration/02_tasks.py
@@ -4,15 +4,21 @@ import logging
 # корневой логгер:
 root = logging.getLogger()
 
+# handlers:
+
+sub_handler = logging.StreamHandler()
+sub_handler.setLevel(logging.DEBUG)
+
 # логгер sub_1 с уровнем INFO:
 sub_1 = logging.getLogger('sub_1')
 sub_1.setLevel(logging.INFO)
+sub_1.addHandler(sub_handler)
 
 # logger sub_2 уровень унаследует от root:
 sub_2 = logging.getLogger('sub_2')
 
 # loger sub_sub_1 с уровнем DEBUG:
-sub_sub_1 = logging.getLogger('sub_2.sub_sub_1')
+sub_sub_1 = logging.getLogger('sub_1.sub_sub_1')
 sub_sub_1.setLevel(logging.DEBUG)
 
 
