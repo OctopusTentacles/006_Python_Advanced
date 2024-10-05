74518107ce3c10418c802e2880aad2876575efc1
Octopus <klinov.artem@gmail.com>
2024-10-05 21:03:54 +0500

    M07_declarative_config_com5

---
+++ Module_07/materials/declarative_approach/03_log_config.py
@@ -0,0 +1,19 @@
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        
+    },
+
+    'handlers': {
+
+    },
+
+    'loggers': {
+
+    },
+
+    # 'filters': {},
+    # 'root': {} # == '': {}
+}
