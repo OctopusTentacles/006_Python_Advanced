731822906a8dbcf1d2d33a4ab3c837543eb01886
Octopus <klinov.artem@gmail.com>
2024-10-06 17:19:15 +0500

    M07_declarative_config_com13

--- Module_07/materials/declarative_approach/03_task.py
+++ Module_07/materials/declarative_approach/03_task.py
@@ -4,7 +4,14 @@ import logging.config
 from log_config import dict_config
 
 
+# применяем конфигурацию:
 logging.config.dictConfig(dict_config)
+
+# определить логгеры:
+root = logging.getLogger('root')
+sub_1 = logging.getLogger('sub_1')
+sub_2 = logging.getLogger('sub_2')
+sub_sub_1 = logging.getLogger('sub_1.sub_sub_1')
 
 def main():
     print("=== Логирование от корневого логгера ===")
