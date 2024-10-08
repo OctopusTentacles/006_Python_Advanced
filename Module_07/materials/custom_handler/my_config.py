9f92d99378a54059aae7fb2ca989aea4c6dbb9c4
Octopus <klinov.artem@gmail.com>
2024-10-08 12:25:22 +0500

    M07_custom_handler_com6

--- Module_07/materials/custom_handler/my_config.py
+++ Module_07/materials/custom_handler/my_config.py
@@ -10,3 +10,16 @@ from my_streamhandler import MyStreamHandler
 
 
 log_dir = os.path.join(os.path.dirname(__file__), 'log_file.log')
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'formatter': {
+            'format': '%(name)s | %(levelname)s | %(message)s | %(module)s,%(funcName)s:%(lineno)d'
+        }
+    },
+
+    'handlers': {}
+}
