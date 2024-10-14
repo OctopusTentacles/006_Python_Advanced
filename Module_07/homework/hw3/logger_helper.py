7a613663ec54978c05dc6379386bd49c645fb276
Octopus <klinov.artem@gmail.com>
2024-10-14 11:59:13 +0500

    M07_hw3_com3 filename

--- Module_07/homework/hw3/logger_helper.py
+++ Module_07/homework/hw3/logger_helper.py
@@ -3,7 +3,22 @@ import sys
 
 
 class LevelFileHandler(logging.Handler):
-    ...
+    def __init__(self, base_filename):
+        super().__init__()
+        self.base_filename = base_filename
+        self.handlers = {}
+
+        # определить имя файла в зависимости от уровня:
+        for level in [
+            logging.DEBUG,
+            logging.INFO,
+            logging.WARNING,
+            logging.ERROR,
+            logging.CRITICAL
+        ]:
+            level_name = logging.getLevelName(level).lower()
+            # имя файла логирования типа calc + _ + debug + .log:
+            filename = f'{self.base_filename}_{level_name}.log'
 
 
 def get_logger(name):
