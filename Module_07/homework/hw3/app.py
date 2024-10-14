43a1e159cb4915dbf90d68636fbffaf8b91c020e
Octopus <klinov.artem@gmail.com>
2024-10-14 11:27:09 +0500

    M07_hw3_com2 prep

---
+++ Module_07/homework/hw3/app.py
@@ -0,0 +1,43 @@
+import logging
+import sys
+
+from utils import string_to_operator
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s || %(name)s || %(levelname)s || %(message)s'
+)
+logger = logging.getLogger('arithmetic_logger')
+
+
+def calc(args):
+    logger.info(f"Arguments: {args}")
+
+    num_1 = args[0]
+    operator = args[1]
+    num_2 = args[2]
+
+    try:
+        num_1 = float(num_1)
+    except ValueError as e:
+        logger.error("Error while converting number 1")
+        logger.debug(e)
+
+    try:
+        num_2 = float(num_2)
+    except ValueError as e:
+        logger.error("Error while converting number 2")
+        logger.debug(e)
+
+    operator_func = string_to_operator(operator)
+
+    result = operator_func(num_1, num_2)
+
+    logger.info(f"Result: {result}")
+    logger.info(f"{num_1} {operator} {num_2} = {result}")
+
+
+if __name__ == '__main__':
+    # calc(sys.argv[1:])
+    calc('2*3')
