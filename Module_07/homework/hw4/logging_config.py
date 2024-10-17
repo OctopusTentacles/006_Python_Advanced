55c53cbb036928cf1360c2b9f2d324fba9a5b515
Octopus <klinov.artem@gmail.com>
2024-10-17 09:20:05 +0500

    M07_hw4_com2 formatters

---
+++ Module_07/homework/hw4/logging_config.py
@@ -0,0 +1,19 @@
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'base': {
+            'format': '%(levelname)s || %(name)s || %(asctime)s || line %(lineno)d || %(message)s'
+        }
+    },
+
+    'handlers': {
+
+    },
+
+    'loggers': {
+
+    },
+}
