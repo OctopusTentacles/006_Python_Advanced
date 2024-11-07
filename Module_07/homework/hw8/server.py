e9c48c669e0d381eba4e8be9e3c044a15deffd52
Octopus <klinov.artem@gmail.com>
2024-11-07 15:16:50 +0500

    M07_hw8_com2 POST

--- Module_07/homework/hw8/server.py
+++ Module_07/homework/hw8/server.py
@@ -3,15 +3,23 @@ from flask import Flask, request
 
 
 app = Flask(__name__)
+log_storage = []
 
 
 @app.route('/log', methods=['POST'])
 def log():
     """
     Записываем полученные логи которые пришли к нам на сервер
-    return: текстовое сообщение об успешной записи, статус код успешной работы
+    return: текстовое сообщение об успешной записи, 
+    статус код успешной работы
 
     """
+    # получить данные в json формате:
+    data = request.json
+    if data:
+        log_storage.append(data)
+        return 'Логи получены', 201
+    return 'Записи логирования не получены', 400
     ...
 
 
