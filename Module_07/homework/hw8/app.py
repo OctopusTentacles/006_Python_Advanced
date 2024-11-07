63a77fafac445a88f5c44d4268abf22c971543c1
Octopus <klinov.artem@gmail.com>
2024-11-07 15:10:01 +0500

    M07_hw8_com1 prep

---
+++ Module_07/homework/hw8/app.py
@@ -0,0 +1,46 @@
+import logging
+import logging.config
+import sys
+
+from utils import string_to_operator
+from logging_config import dict_config
+
+
+logging.config.dictConfig(dict_config)
+logger = logging.getLogger('arithmetic_logger')
+
+
+def calc(args):
+    logger.info(f"Arguments: {args}")
+
+    num_1 = args[0]
+    operator = args[1]
+    num_2 = args[2]
+
+    try:
+        num_1 = float(num_1)
+    except ValueError as e:
+        logger.error("Error while converting number 1")
+        logger.debug(e)
+
+    try:
+        num_2 = float(num_2)
+    except ValueError as e:
+        logger.error("Error while converting number 2")
+        logger.debug(e)
+
+    operator_func = string_to_operator(operator)
+
+    result = operator_func(num_1, num_2)
+
+    logger.info(f"Result: {result}")
+    logger.info(f"{num_1} {operator} {num_2} = {result}")
+
+
+if __name__ == '__main__':
+    calc('2*3')
+    logger.debug("This is a debug message")
+    logger.debug("This is a debug message NOT ASCII ÎŒØ∏‡°⁄·°€йцукен")
+
+    logger.info("This is an info message")
+    logger.info("This is an info message NOT ASCII ÎŒØ∏‡°⁄·°€йцукен")
---
+++ Module_07/homework/hw8/logging_config.py
@@ -0,0 +1,126 @@
+import logging
+import logging.config
+import os
+
+from contextlib import redirect_stdout
+from logging import LogRecord
+from logging.handlers import TimedRotatingFileHandler
+from logging_tree import printout
+
+
+log_dir = os.path.join(os.path.dirname(__file__), 'logs')
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+# ===================================================================
+# class logging.Filter - базовый класс для создания фильтров логирования.
+# def filter() - пропускает все сообщения, возвращая True.
+# - создать класс, наследуемый от logging.Filter,
+# - настроить метод filter() на проверку не ASCII-символов.
+class ASCIIFilter(logging.Filter):
+    """Фильтр для отбора сообщений, содержащих только ASCII-символы."""
+    def filter(self, record: LogRecord) -> bool:
+        """
+        Определяет, содержатся ли в сообщении логирования только ASCII-символы.
+
+        Args:
+            record (LogRecord): Объект лог-записи, содержащий информацию о сообщении логирования.
+
+        Returns:
+            bool: True, если сообщение содержит только ASCII-символы, иначе False.
+        """
+        return record.getMessage().isascii()
+
+# хочу записывать логи с не ASCII символами в отдельный журнал
+class NonASCIIFilter(logging.Filter):
+    """Фильтр для отбора сообщений, содержащих не-ASCII символы."""
+    def filter(self, record: LogRecord) -> bool:
+        """
+        Определяет, содержатся ли в сообщении логирования не-ASCII символы.
+
+        Args:
+            record (LogRecord): Объект лог-записи, содержащий информацию о сообщении логирования.
+
+        Returns:
+            bool: True, если сообщение содержит не-ASCII символы, иначе False.
+        """
+        return not record.getMessage().isascii()
+# ===================================================================
+
+dict_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'filters': {
+        'ascii_filter': {
+            '()': 'logging_config.ASCIIFilter'
+        },
+        'non_ascii_filter': {
+            '()': 'logging_config.NonASCIIFilter'
+        },
+    },
+
+    'formatters': {
+        'base': {
+            'format': '%(levelname)-8s || %(name)-10s || %(asctime)s || line %(lineno)-4d || %(message)s'
+        }
+    },
+
+    'handlers': {
+        'file_handler': {
+            'class': 'logger_helper.LevelFileHandler',
+            'level': 'DEBUG',
+            'formatter': 'base',
+            'base_filename': os.path.join(log_dir, 'calc'),
+            'filters': ['ascii_filter'],
+        },
+
+        'rotating_handler': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'level': 'INFO',
+            'formatter': 'base',
+            'filename': os.path.join(log_dir, 'utils.log'),
+            'when': 'S',
+            'interval': 10,
+            'backupCount': 2,
+            'encoding': 'utf8',
+            'filters': ['ascii_filter'],
+        },
+        'non_ascii_handler': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'base',
+            'filename': os.path.join(log_dir, 'non_ascii.log'),
+            'filters': ['non_ascii_filter'],
+        },
+    },
+
+    'loggers': {
+        'arithmetic_logger': {
+            'level': 'DEBUG',
+            'handlers': ['file_handler', 'non_ascii_handler'],
+            'propagate': False,
+        },
+        'operators_logger': {
+            'level': 'DEBUG',
+            'handlers': ['file_handler', 'non_ascii_handler'],
+            'propagate': False,
+        },
+        'utils': {
+            'level': 'INFO',
+            'handlers': ['rotating_handler', 'non_ascii_handler'],
+            'propagate': False,
+        }
+    }
+}
+
+# настройка логгеров, иначе в дереве видно только root:
+logging.config.dictConfig(dict_config)
+# ===================================================================
+# файл для структуры логгеров:
+with open(os.path.join(log_dir, 'logging_tree.txt'), 'w') as file:
+    with redirect_stdout(file):
+        # func printout выводит дерево логов:
+        printout()
+# ===================================================================
