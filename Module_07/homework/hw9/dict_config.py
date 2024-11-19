264b31f16f7247b6581b4c157349d73bcd8cbd67
Octopus <klinov.artem@gmail.com>
2024-11-19 08:01:31 +0500

    M07_hw9_com2

--- Module_07/homework/hw9/dict_config.py
+++ Module_07/homework/hw9/dict_config.py
@@ -1,1 +1,26 @@
 # TODO переписать реализацию ini-файла в формате dict-конфигурации.
+
+{
+    'loggers': {
+        'keys': ['root', 'appLogger']
+    },
+
+    'handlers': {
+        'keys': ['consoleHandler', 'fileHandler']
+    },
+
+    'formatters': {
+        'keys': ['fileFormatter', 'consoleFormatter']
+    },
+
+
+
+
+
+    'loggers': {
+        'root': {
+            'level': 'DEBUG',
+            'handlers': 'consoleHandler',
+        },
+    }
+}
