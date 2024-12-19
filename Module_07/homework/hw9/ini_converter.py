6a7adeb1ef21028525baaa02aa718d486473e789
Octopus <klinov.artem@gmail.com>
2024-12-19 13:41:03 +0500

    M07_hw9_com9

--- Module_07/homework/hw9/ini_converter.py
+++ Module_07/homework/hw9/ini_converter.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 
 
 def ini_to_dict(ini_file_path):
@@ -11,4 +12,8 @@ def ini_to_dict(ini_file_path):
     Returns:
         dict: Конфигурация в формате словаря.
     """
-    
+
+    file_path = os.path.join(os.path.dirname(__file__), ini_file_path)
+
+    config = configparser.ConfigParser()
+    config.read(file_path)
